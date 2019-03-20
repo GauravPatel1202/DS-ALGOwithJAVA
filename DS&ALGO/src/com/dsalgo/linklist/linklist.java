1bad3bab60b69084e5959cb0fb15caf6db0cc63e
Gaurav Patel <DeepakDevulapalli@LiFung.com>
2019-03-20 12:26:22 +0530

    Start

---
+++ DS&ALGO/src/com/dsalgo/linklist/linklist.java
@@ -0,0 +1,10 @@
+package com.dsalgo.linklist;
+
+public class linklist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DS&ALGO/src/com/dsalgo/queue/myQueue.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.dsalgo.queue;
+
+/**
+ * @author pc
+ *
+ */
+public class myQueue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
