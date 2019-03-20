acb2366779dc930b631f374e32c969046897cdda
Gaurav Patel <DeepakDevulapalli@LiFung.com>
2019-03-20 12:29:37 +0530

    Start

---
+++ DS&ALGO/src/com/dsalgo/graph/graph.java
@@ -0,0 +1,10 @@
+package com.dsalgo.graph;
+
+public class graph {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DS&ALGO/src/com/dsalgo/tree/tree.java
@@ -0,0 +1,10 @@
+package com.dsalgo.tree;
+
+public class tree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
